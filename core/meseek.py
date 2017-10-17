df23cdc0a714bb35cfb0ce0586e559d0d0a1369d
realsumit <sumitsarinofficial@gmail.com>
2017-10-17 15:58:10 +0530

    minor tweeks

--- core/meseek.py
+++ core/meseek.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python
 
 import sys
-from fetcher import fetcher
 import subprocess
-from subprocess import run
+from fetcher import fetcher
+from openurl import openurl
 
 class bcolors:
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    ENDC = '\033[0m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	ENDC = '\033[0m'
 
 def main():
 	print(bcolors.RED + "Running Meseek v1.0.0" + bcolors.ENDC)
 
-	process = subprocess.Popen(["ls", "-l"],shell=True, stdout=subprocess.PIPE)
-	process.wait()
-	print(process.stdout.read())
-
-	results = fetcher(sys.argv[1])
-	answer = 1
+	fixlist = fetcher(sys.argv[1])
+	decision = 1
 	fix_counter = 1
-	while (answer!=0):
+	
+	while (decision!=0):
+		
 		print("Please select an option:")
-		print("1. Open Fix (" + str(fix_counter) + '/' + str(len(results)) + ')')
+		print("1. Open Fix (" + str(fix_counter) + '/' + str(len(fixlist)) + ')')
+		print("2. Share Fix with Meseek")
 		print("0. Exit")
-		answer = int(input())
-		if (answer==1):
-			#open a browser her
-			#ask the dude did it work?
+		decision = int(input())
+		
+		if (decision==1):
+			url = fixlist[fix_counter-1]
+			openurl(url)
 			fix_counter += 1
-			if (fix_counter==len(results)+1):
+			if (fix_counter==len(fixlist)+1):
 				print("Meseek is out of solutions :(")
-				answer = 0
+				decision = 0
+		
+		elif (decision==2):
+			#Write code here
+			decision=0
 
 
 
