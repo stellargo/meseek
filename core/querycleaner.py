ed9cdb670287a14b8e0768f0fb9b99f022958114
realsumit <sumitsarinofficial@gmail.com>
2017-10-18 15:24:45 +0530

    querycleaner.py

--- core/querycleaner.py
+++ core/querycleaner.py
@@ -1,4 +1,10 @@
 def querycleaner(query):
+	query = str(query)
+	querylen = len(query)
+	query = query[2:querylen-1]
+	query = query.replace('\\n','\n')
+	print(query)
+	query = query.replace('\n',' ')
 	query = query.replace('. ',' ')
 	query = query.replace(': ',' ')
 	return query
