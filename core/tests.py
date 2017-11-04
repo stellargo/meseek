6dee884c78dd88a5df8a077b684ee52667393229
realsumit <sumitsarinofficial@gmail.com>
2017-11-04 11:57:35 +0530

    tests.py: updated

--- core/tests.py
+++ core/tests.py
@@ -5,9 +5,9 @@ class Test(unittest.TestCase):
 	def testStringMatcher(self):
 		A = 'book'
 		B = 'books'
-		if (int(stringMatcher(A,B))==80):
-			self.fail("selectionSort method fails.")
+		if (stringMatcher(A,B)!=80.0):
+			self.fail("stringMatcher method fails.")
 		A = 'book'
 		B = 'abcd'
-		if (int(stringMatcher(A,B))==0):
-			self.fail("selectionSort method fails.")
+		if (stringMatcher(A,B)!=0.0):
+			self.fail("stringMatcher method fails.")
